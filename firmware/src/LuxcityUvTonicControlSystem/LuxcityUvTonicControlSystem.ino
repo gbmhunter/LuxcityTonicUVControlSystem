34aa6a71fc8d01e0f01d0b4fddf3f131365f953c
Geoffrey Hunter <gbmhunter@gmail.com>
2012-10-10 23:39:41 +1300

    All 8 relay shields are controllable from Vixen (channels 1 thru 64).

--- firmware/src/LuxcityUvTonicControlSystem/LuxcityUvTonicControlSystem.ino
+++ firmware/src/LuxcityUvTonicControlSystem/LuxcityUvTonicControlSystem.ino
@@ -36,7 +36,19 @@
  
 #define END_OF_MSG_CHAR 'n'
 #define NUM_CHANNELS 64          //!< Number of control channels
-#define NUM_RELAY_CONTROL_BOARDS
+#define NUM_RELAY_CONTROL_BOARDS 8
+
+typedef enum
+{
+  RELAY_BOARD_1,
+  RELAY_BOARD_2,
+  RELAY_BOARD_3,
+  RELAY_BOARD_4,
+  RELAY_BOARD_5,
+  RELAY_BOARD_6,
+  RELAY_BOARD_7,
+  RELAY_BOARD_8
+} relayDriverShieldNum_t;
 
 //! Array to hold the I2C addresses of the relay control boards
 uint8_t i2cAddresses[NUM_RELAY_CONTROL_BOARDS] = 
@@ -51,6 +63,8 @@ uint8_t i2cAddresses[NUM_RELAY_CONTROL_BOARDS] =
   0x27
 };
 
+void SetRelayStates(uint8_t relayDriverShieldNum, uint8_t relayStates);
+
 String inputString = "";         //!< A string to hold incoming data
 boolean stringComplete = false;  //!< Whether the string is complete
 
@@ -61,43 +75,115 @@ void setup() {
   inputString.reserve(NUM_CHANNELS + 1);
   
   Wire.begin(); // Wake up I2C bus
-
-  // Set addressing style
-  Wire.beginTransmission(i2cAddresses[0]);
-  Wire.write(0x12);
-  Wire.write(0x20); // use table 1.4 addressing
-  Wire.endTransmission();
-
-  // Set I/O bank A to outputs
-  Wire.beginTransmission(i2cAddresses[0]);
-  Wire.write(0x00); // IODIRA register
-  Wire.write(0x00); // Set all of bank A to outputs
-  Wire.endTransmission();
+  uint8_t i = 0;
+  for(i = 0; i < 8; i++)
+  {
+    // Set addressing style
+    Wire.beginTransmission(i2cAddresses[i]);
+    Wire.write(0x12);
+    Wire.write(0x20); // use table 1.4 addressing
+    Wire.endTransmission();
+  
+    // Set I/O bank A to outputs
+    Wire.beginTransmission(i2cAddresses[i]);
+    Wire.write(0x00); // IODIRA register
+    Wire.write(0x00); // Set all of bank A to outputs
+    Wire.endTransmission();
+  }
   
   //! @debug
   pinMode(13, OUTPUT);
 }
 
-void loop() {
+
+
+void loop() 
+{
   // print the string when a newline arrives:
-  if (stringComplete) {
+  if (stringComplete) 
+  {
     // Process string
     int i;
+    
+    // Start with board 1
+    uint8_t relayDriverShieldNum = RELAY_BOARD_1;
+    uint8_t relayNum = 0;
+    uint8_t relayStates = 0;
+    
     for(i = 0; i < NUM_CHANNELS; i++)
     {
+          // Check for any value other than 0 to turn relay on. If 0, leave alone
+          if(inputString[i] != 1)
+          {
+            //digitalWrite(13, HIGH);
+            // Set relay on
+            relayStates = relayStates | (1 << relayNum);
+          }
+          
+          // Check if setting the last relay on the board (8 per board)
+          if(relayNum == 7)
+          {
+            //digitalWrite(13, LOW);
+            // Send change relay state command to correct board
+            SetRelayStates(relayDriverShieldNum, relayStates);
+            // Reset relayStates for next 8 values to process
+            relayStates = 0;
+            // Reset relay number
+            relayNum = 0;
+            // Move to next board
+            relayDriverShieldNum = relayDriverShieldNum +1;
+            
+          }
+          else
+          {
+            // Increment board relay number
+            relayNum++;
+          }
+          
+          
+     }
+     
+      /*
       if(inputString[i] != 0)
-    {
-      digitalWrite(13, HIGH);
-    }
-    else
-      digitalWrite(13, LOW);
-    }
+      {
+        digitalWrite(13, HIGH);
+        if(i == 0)
+        {
+          sendValueToLatch(1); 
+        }
+      }
+      else
+      {
+        digitalWrite(13, LOW);
+        if(i == 0)
+        {
+          sendValueToLatch(0); 
+        }
+      }
+      */
+   
     
     //Serial.println(inputString); 
     // Clear the string for the next message
     inputString = "";
     stringComplete = false;
   }
+}
+
+void SetRelayStates(uint8_t relayDriverShieldNum, uint8_t relayStates)
+{
+  Wire.beginTransmission(i2cAddresses[relayDriverShieldNum]);
+  Wire.write(0x12);        // Select GPIOA
+  Wire.write(relayStates);  // Send value to bank A
+  Wire.endTransmission();
+}
+
+void sendValueToLatch(int latchValue)
+{
+  Wire.beginTransmission(i2cAddresses[0]);
+  Wire.write(0x12);        // Select GPIOA
+  Wire.write(latchValue);  // Send value to bank A
+  Wire.endTransmission();
 }
 
 /*
